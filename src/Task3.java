c23b7a8de77a264dbadc8c64576438fc5839e8bd
Dinesh Kumar <dinnukumar.974@gmail.com>
2022-08-08 16:50:33 +0530

    task3

--- src/Task3.java
+++ src/Task3.java
@@ -4,11 +4,12 @@ public class Task3 {
         Scanner sc=new Scanner(System.in);
         char a=sc.next().charAt(0);
         char b=sc.next().charAt(0);
-       for(int i=0;i<3;i++){
+        int n=sc.nextInt();
+       for(int i=0;i<n;i++){
            for(int k=0;k<=i;k++){
                System.out.print(a);
            }
-           for(int j=3;j>i;j--){
+           for(int j=3;j>=i;j--){
                System.out.print(b);
            }
             System.out.println();
