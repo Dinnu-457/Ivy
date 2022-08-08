e745b0481e771d52a465cf89bc0b37622407b431
Dinesh Kumar <dinnukumar.974@gmail.com>
2022-08-08 15:25:35 +0530

    task1 added

--- src/Task1.java
+++ src/Task1.java
@@ -9,19 +9,36 @@ public class Task1 {
         int dif1=(x-n);
         int dif2=(y-n);
         int dif3=(z-n);
-        if(dif1>=0 && dif2>=0 && dif3>=0) {
-            if (dif1 <= dif2 && dif1 <= dif3) {
-                System.out.println("L1");
-            } else if (dif2 <= dif1 && dif2 <= dif3) {
+        if(dif1<0 && dif2>=0 && dif3>=0 ){
+            if(dif2<dif3){
                 System.out.println("L2");
-            } else if (dif3 <= dif1 && dif3 <= dif2) {
+            }else{
                 System.out.println("L3");
             }
         }
-        else{
-            System.out.println("No one");
+        if(dif1>=0 && dif2<0 && dif3>=0 ){
+            if(dif1<dif3){
+                System.out.println("L1");
+            }else{
+                System.out.println("L3");
+            }
         }
-
+        if(dif1>=0 && dif2>=0 && dif3<0 ){
+            if(dif1<dif2){
+                System.out.println("L1");
+            }else{
+                System.out.println("L2");
+            }
+        }
+        if(dif1<=dif2 && dif1<=dif3 && dif1>=0 && dif2>=0 && dif3>=0){
+            System.out.println("L1");
+        }
+        else if(dif2<=dif1 && dif2<=dif3 && dif1>=0 && dif2>=0 && dif3>=0){
+            System.out.println("L2");
+        }
+        else if(dif3<=dif1 && dif3<=dif2 && dif1>=0 && dif2>=0 && dif3>=0){
+            System.out.println("L3");
+        }
 
 
     }
