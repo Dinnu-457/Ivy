c3965b928f046bcaa2bfc615e5dc799c9d217471
Dinesh Kumar <dinnukumar.974@gmail.com>
2022-08-09 13:02:48 +0530

    OneD

--- src/Day2/OneD.java
+++ src/Day2/OneD.java
@@ -13,9 +13,11 @@ public class OneD {
         for(int i=0;i<size;i++){
             a[i]=sc.nextInt();
             sum+=a[i];
-            count[a[i]]++;
+            count[a[i]]++;//for tracking mode
         }
+        //mean
         System.out.println("mean :"+sum/size);
+        //median
         if(size%2==0){
             int median=a[size/2]+a[(size/2)+1];
             System.out.println("median "+median/2);
@@ -23,6 +25,7 @@ public class OneD {
         else{
             System.out.println("median "+a[size/2]);
         }
+        //mode
         int max=Integer.MIN_VALUE;
         for(int i=0;i<10;i++) {
             if (count[i] > max) {
