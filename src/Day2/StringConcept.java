5b0d557167636f5118141dcb457e1105d200ab8f
Dinesh Kumar <dinnukumar.974@gmail.com>
2022-08-09 12:22:32 +0530

    added prog2

---
+++ src/Day2/StringConcept.java
@@ -0,0 +1,20 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class StringConcept {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+        System.out.println("length of 2 strings "+s1.length()+","+s2.length());
+        System.out.println("concatenation of 2 strings: "+s1.concat(s2));
+        String a[]=s1.split("@",1);
+        for(String i:a){
+            System.out.println(i);
+        }
+        StringBuffer sb=new StringBuffer(s1);
+        System.out.print(sb.reverse());
+
+    }
+}
